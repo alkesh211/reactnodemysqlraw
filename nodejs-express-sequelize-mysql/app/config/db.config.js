74bbc468ed531b3da2a8b2d4860eaf1fa07cccd7
Alkesh <alkesh@vibrant-info.com>
2023-03-23 15:21:28 +0530

    Add node express mysql sequelize

---
+++ nodejs-express-sequelize-mysql/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "WJxJfdn40f3Soc0nkpzne0Hiejg",
+  DB: "testdb",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
